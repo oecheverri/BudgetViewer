c0f04fc4894699c37b0407274d2e523d01fce101
Oscar Echeverri <oscar@echeverri.ca>
2021-02-12 17:10:55 -0500

    Converted Project to SwiftUI Lifecycle

---
+++ BudgetViewer/App/BudgetViewerApp.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetViewerApp.swift
+//  BudgetViewer
+//
+//  Created by Oscar Echeverri on 2021-02-12.
+//  Copyright © 2021 Oscar Echeverri. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BudgetViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
